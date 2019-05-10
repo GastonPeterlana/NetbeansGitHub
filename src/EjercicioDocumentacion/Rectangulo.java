ea018af802191d1b753bf0459b7e77fbcca2979e
Gaston Peterlana <gastonpeterlana@gmail.com>
2019-05-10 09:02:32 +0200

    Commit clase RECTANGULO

---
+++ src/EjercicioDocumentacion/Rectangulo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjercicioDocumentacion;
+
+/**
+ *
+ * @author 6J
+ */
+public class Rectangulo {
+    float alto, ancho;
+
+    public Rectangulo() {
+        alto=11;
+        ancho=12;                
+    }
+    
+}
